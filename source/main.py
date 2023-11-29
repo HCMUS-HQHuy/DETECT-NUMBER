8829d1cb9b170cb611ec712b9cd0dfbd5734377e
Huynh Quoc Huy <huynhquochuy06012005@gmail.com>
2023-11-29 09:01:16 +0700

    update out main

--- source/main.py
+++ source/main.py
@@ -3,9 +3,16 @@ import Vectorization
 import Downsampling
 import Histogram
 
+def outAns(data1, data2, f):
+    print("vector_train: ", f(data1))
+    print("vector_test : ", f(data2))
+
 X_train, y_train = load_mnist('data/', kind = 'train')
 X_test, y_test = load_mnist('data/', kind = 't10k')
 
-# array = Vectorization.get(X_train)
-# print(array)
-# array = Vectorization.get(X_test)
+print("Vectorization: ")
+outAns(X_train, X_test, Vectorization.get)
+print("Dowsampling: ")
+outAns(X_train, X_test, Downsampling.get)
+print("Histogram: ")
+outAns(X_train, X_test, Histogram.get)
