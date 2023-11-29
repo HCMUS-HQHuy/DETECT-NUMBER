8713c30d2ee843661fa3a14ec908b2e2fbfb5633
NTK <88838237+ilovephysic@users.noreply.github.com>
2023-11-29 20:21:19 +0700

    Add files via upload

--- source/Downsampling.py
+++ source/Downsampling.py
@@ -1,13 +1,19 @@
 import numpy 
-from Vectorization import vectorization
+from normalize import normalize
 
 def Downsample(img): 
-    vector = numpy.zeros(784)
-    
+    n=img.shape[0]                      #28 phan tu
+    vector=numpy.zeros(14*14)
+    index=0
+    for i in range(0,n,2):          
+       for j in range(0,n,2):
+          vector[index]=(img[i,j] + img[i,j+1] + img[i+1,j] + img[i+1,j+1])/4
+          index=index+1
     return vector
 
 def get(data):
-    arrayVector = numpy.zeros((data.shape[0], data[0].shape[0] * data[1].shape[1]))
-    for i in range(data.shape[0]):
-        arrayVector[i] = Downsample(data[i])
-    return arrayVector
+   arrayVector=numpy.zeros(data.shape[0], 14*14)
+   for i in range(0, data.shape[0]):
+      arrayVector[i]=Downsample(data[i])
+    
+   return arrayVector
