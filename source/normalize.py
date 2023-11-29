4ecfa69324e6e78ca64d19fc0c58a25fa17e3977
NTK <88838237+ilovephysic@users.noreply.github.com>
2023-11-29 19:47:30 +0700

    Add files via upload

--- source/normalize.py
+++ source/normalize.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def normalize(data):
-    n=data.shape[0]
+def normalize(one_img):             #one_image la mang data[1]
+    n=one_img.shape[0]
     after_normalize=np.zeros(n,n)
     for i in range (0,n):
         for j in range (0,n):
-            after_normalize[i][j]=data[i][j]/255
+            after_normalize[i][j]=one_img[i][j]/255
     return after_normalize
 
     
