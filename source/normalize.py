ad839543d9d39b62909e4a62b840412c5248bd9a
Chau57 <139162330+Chau57@users.noreply.github.com>
2023-12-02 10:28:00 +0700

    Update normalize.py

--- source/normalize.py
+++ source/normalize.py
@@ -2,10 +2,8 @@ import numpy as np
 
 def normalize(one_img):             #one_image la mang data[1]
     n=one_img.shape[0]
-    after_normalize=np.zeros(n,n)
-    for i in range (0,n):
-        for j in range (0,n):
-            after_normalize[i][j]=one_img[i][j]/255
+    after_normalize=np.zeros(n*n)
+    after_normalize = numpy.divide(one_img,255)
     return after_normalize
 
     
